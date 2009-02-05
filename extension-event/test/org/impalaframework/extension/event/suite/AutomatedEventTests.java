6e1c2447f7d1fded76a5937e2900979d3e7febf9
philzoio <philzoio@realsolve.co.uk>
2009-02-05 11:01:25 +0000

    Initial build

--- extension-event/test/org/impalaframework/extension/event/suite/AutomatedEventTests.java
+++ extension-event/test/org/impalaframework/extension/event/suite/AutomatedEventTests.java
@@ -12,7 +12,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class AllEventTests extends TestCase {
+public class AutomatedEventTests extends TestCase {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite();
