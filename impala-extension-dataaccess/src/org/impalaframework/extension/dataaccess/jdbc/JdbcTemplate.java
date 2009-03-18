9c13283e57f911d6306977b679d72fb74d0d0f72
philzoio <philzoio@realsolve.co.uk>
2009-03-18 21:36:01 +0000

    Comments

--- impala-extension-dataaccess/src/org/impalaframework/extension/dataaccess/jdbc/JdbcTemplate.java
+++ impala-extension-dataaccess/src/org/impalaframework/extension/dataaccess/jdbc/JdbcTemplate.java
@@ -8,13 +8,20 @@ import javax.sql.DataSource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ColumnMapRowMapper;
 
+/**
+ * Extension of {@link JdbcTemplate} which contains method
+ * {@link #queryForList(String, Map)}, which takes named arguments in the form
+ * of a {@link Map} instance
+ * 
+ * @author Phil Zoio
+ */
 public class JdbcTemplate extends org.springframework.jdbc.core.JdbcTemplate {
-	
+
 	private JdbcArgumentsConverter converter;
-	
+
 	private String parameterPrefix = null;
 	private String parameterSuffix = null;
-	
+
 	public JdbcTemplate() {
 		super();
 	}
@@ -22,37 +29,60 @@ public class JdbcTemplate extends org.springframework.jdbc.core.JdbcTemplate {
 	public JdbcTemplate(DataSource dataSource) {
 		super(dataSource);
 	}
-	
+
 	@Override
 	public void afterPropertiesSet() {
 		super.afterPropertiesSet();
-		
+
 		if (this.parameterPrefix == null)
 			this.parameterPrefix = "${";
 		if (this.parameterSuffix == null)
 			this.parameterSuffix = "}";
 		converter = new JdbcArgumentsConverter(parameterPrefix, parameterSuffix);
 	}
-	
-	
+
+	public List<Map<String, Object>> queryForList(String sql,
+			Map<String, Object> parameters) throws DataAccessException {
+		return queryForList(sql, parameters, false);
+	}
+
+	/**
+	 * Returns list of maps as in {@link #queryForList(String, Object[])}.
+	 * 
+	 * @param sql
+	 *            the sql which contains named arguments, by default in the form
+	 *            ${name}, which will be replaced by ? before being submitted to
+	 *            the JDBC driver
+	 * @param parameters
+	 *            a map of parameter arguments
+	 * @param resultKeysAsLowerCase
+	 *            if true all result keys will be returned as lower case
+	 * @return a list of maps as in {@link #queryForList(String, Object[])}
+	 * @throws DataAccessException
+	 */
 	@SuppressWarnings("unchecked")
-	public List<Map<String,Object>>queryForList(String sql, Map<String,Object> parameters) throws DataAccessException {
-		
+	public List<Map<String, Object>> queryForList(String sql,
+			Map<String, Object> parameters, boolean resultKeysAsLowerCase)
+			throws DataAccessException {
+
 		final JdbcArguments convert = converter.convert(sql, parameters);
-		return query(convert.getSql(), convert.getArguments(), new ColumnMapRowMapper() {
+		final ColumnMapRowMapper rowMapper = resultKeysAsLowerCase ? new ColumnMapRowMapper() {
 
 			@Override
 			protected String getColumnKey(String columnName) {
 				return super.getColumnKey(columnName).toLowerCase();
 			}
-			
-		});
+
+		}
+				: new ColumnMapRowMapper();
+
+		return query(convert.getSql(), convert.getArguments(), rowMapper);
 	}
 
 	public void setParameterPrefix(String parameterPrefix) {
 		this.parameterPrefix = parameterPrefix;
 	}
-	
+
 	public void setParameterSuffix(String parameterSuffix) {
 		this.parameterSuffix = parameterSuffix;
 	}
