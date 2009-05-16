abf03b51d5a624152a3da9eebf8eac8e9f74c985
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@447e0356-f1d6-11dd-b507-8b4ecd5210be>
2009-05-16 08:57:36 +0000

    Renamed annotation method adapter

--- trunk/impala-extension-mvc/src/org/impalaframework/extension/mvc/DefaultAnnotationHandlerAdapter.java
+++ trunk/impala-extension-mvc/src/org/impalaframework/extension/mvc/DefaultAnnotationHandlerAdapter.java
@@ -2,7 +2,7 @@ package org.impalaframework.extension.mvc;
 
 import org.springframework.web.bind.support.WebArgumentResolver;
 
-public class DefaultAnnotationHandlerAdapter extends RequestPreservingAnnotationHandlerAdapter {
+public class DefaultAnnotationHandlerAdapter extends FlashStateEnabledAnnotationHandlerAdapter {
 
 	public void init() {
 		setCustomArgumentResolvers(new WebArgumentResolver[]{
