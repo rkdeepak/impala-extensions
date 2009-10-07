4fb3c8ecea8af1043b712bcc9aab2245f1d9d9eb
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@447e0356-f1d6-11dd-b507-8b4ecd5210be>
2009-10-07 09:24:40 +0000

    Added exception logging to event task

--- trunk/impala-extension-event/src/org/impalaframework/extension/event/EventTask.java
+++ trunk/impala-extension-event/src/org/impalaframework/extension/event/EventTask.java
@@ -1,11 +1,18 @@
 package org.impalaframework.extension.event;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.impalaframework.exception.ExecutionException;
 import org.springframework.util.Assert;
 
 /**
+ * Task which will holds an {@link EventListener}, and invokes the {@link EventListener#onEvent(Event)} method
+ * when the {@link #run()} method is called.
  */
 public class EventTask implements Runnable {
 
+	private static final Log logger = LogFactory.getLog(EventTask.class);
+	
 	final private Event Event;
 
 	final private EventListener eventListener;
@@ -21,7 +28,16 @@ public class EventTask implements Runnable {
 	}
 
 	public void run() {
-		eventListener.onEvent(Event);
+		try {
+			eventListener.onEvent(Event);
+		} catch (Throwable e) {
+			logger.error("Uncaught error running event: " + Event + ". Message: " + e.getMessage(), e);
+			if (e instanceof RuntimeException) {
+				throw (RuntimeException) e;
+			} else {
+				throw new ExecutionException(e);
+			}
+		}
 	}
 
 	protected Event getEvent() {
