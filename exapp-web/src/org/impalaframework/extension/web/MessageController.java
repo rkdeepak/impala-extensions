b50c6e51e9f04f8ba74e4cdee778d38821586ac3
philzoio <philzoio@realsolve.co.uk>
2010-03-22 14:47:38 +0000

    Fixed up example for extensions project

--- exapp-web/src/org/impalaframework/extension/web/MessageController.java
+++ exapp-web/src/org/impalaframework/extension/web/MessageController.java
@@ -1,30 +1,40 @@
 package org.impalaframework.extension.web;
 
-import java.util.HashMap;
+import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
-
+import org.impalaframework.extension.mvc.annotation.handler.LightweightAdaptable;
 import org.impalaframework.extension.root.MessageService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
-public class MessageController  implements Controller {
+@org.springframework.stereotype.Controller
+@LightweightAdaptable
+@SuppressWarnings("unchecked")
+public class MessageController {
+	
 	private MessageService messageService;
+	
+	@ModelAttribute("className")
+	public String getClassName() {
+		return MessageController.class.getName();
+	}
+
+	@ModelAttribute("timestamp")
+	public Long getTimestamp() {
+		return System.currentTimeMillis();
+	}
+	
+	@RequestMapping("/message.htm")
+	public String message(
+			@ModelAttribute("className") String className,
+			@ModelAttribute("timestamp") Long timestamp,
+			Map model) {
+		
+		model.put("message", className + " (" + timestamp + "): " + messageService.getMessage());
+		return "test";
+	}
 
 	public void setMessageService(MessageService messageService) {
 		this.messageService = messageService;
-	}
-
-	public ModelAndView handleRequest(HttpServletRequest arg0,
-			HttpServletResponse arg1) throws Exception {
-		HashMap<String, String> map = new HashMap<String, String>();
-		map.put("message", messageService.getMessage());
-
-		ModelAndView mav = new ModelAndView("test", map);
-		return mav;
 	}
 }
