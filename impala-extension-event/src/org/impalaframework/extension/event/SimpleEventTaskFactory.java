0b49ff07c42afa7a7e1ecb7c970fd5d7463b8edf
philzoio <philzoio@realsolve.co.uk>
2010-07-13 19:55:47 +0000

    JavaDocs

--- impala-extension-event/src/org/impalaframework/extension/event/SimpleEventTaskFactory.java
+++ impala-extension-event/src/org/impalaframework/extension/event/SimpleEventTaskFactory.java
@@ -1,6 +1,10 @@
 package org.impalaframework.extension.event;
 
-
+/**
+ * Implementation of {@link EventTaskFactory} which simply returns an {@link EventTask}
+ * instance.
+ * @author Phil Zoio
+ */
 public class SimpleEventTaskFactory implements EventTaskFactory {
 
 	public SimpleEventTaskFactory() {
