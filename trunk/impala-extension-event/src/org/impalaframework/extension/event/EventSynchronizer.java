4f7bff186cf4669d73de40431f2b335fadb36353
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@447e0356-f1d6-11dd-b507-8b4ecd5210be>
2010-07-13 19:55:47 +0000

    JavaDocs

--- trunk/impala-extension-event/src/org/impalaframework/extension/event/EventSynchronizer.java
+++ trunk/impala-extension-event/src/org/impalaframework/extension/event/EventSynchronizer.java
@@ -7,6 +7,7 @@ package org.impalaframework.extension.event;
 public interface EventSynchronizer {
 	
 	void awaitTransactionCompletion(EventTask Event);
+	
 	void submitTask(EventTask eventTask);
 	
 }
