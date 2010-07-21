6b7a3b1f9969a7a4e51b907d1d55b1fffaa8581f
philzoio <philzoio@realsolve.co.uk>
2010-07-21 08:37:42 +0000

    Logging/JavaDoc

--- impala-extension-event/src/org/impalaframework/extension/event/SynchronousEventService.java
+++ impala-extension-event/src/org/impalaframework/extension/event/SynchronousEventService.java
@@ -2,9 +2,19 @@ package org.impalaframework.extension.event;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.util.Assert;
 
+/**
+ * Implementation of {@link EventService} which synchronously calls through to 
+ * registered event listeners, passing through the {@link Event} instance to each.
+ * Exceptions are not caught: instead, they are propagated to the original caller.
+ * @author Phil Zoio
+ */
 public class SynchronousEventService implements EventService {
+
+	private static final Log logger = LogFactory.getLog(AsynchronousEventService.class);
 
 	private EventListenerRegistry eventListenerRegistry;
 	
@@ -15,9 +25,20 @@ public class SynchronousEventService implements EventService {
 		EventType eventType = event.getEventType();
 		String type = eventType.getType();
 		List<EventListener> list = eventListenerRegistry.getEventListeners(type);
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug("Registered synchronous listeners registered for type " + type + ": " + list);
+		}
 
 		if (list != null) {
 			for (EventListener eventListener : list) {
+				
+				if (logger.isDebugEnabled()) {
+					logger.debug("Executing onEvent for listener: " + eventListener);
+				}
+				
+				//note: exceptions not caught as they are propagated to original caller
+				//can do this because this is synchronous
 				eventListener.onEvent(event);
 			}
 		}
